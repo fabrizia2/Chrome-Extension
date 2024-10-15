fcc2b3422b624bdba2ca814d8c6e5cf8364939dc
iakev <kirimikmwongera@gmail.com>
2024-10-15 18:01:23 +0300

    added an alarm to discard inactive tabs more than 30 minutes

--- background.js
+++ background.js
@@ -1,4 +1,24 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.log('Extension installed');
-  });
-  
+  console.log('Extension installed');
+});
+
+// Function to discard inactive tabs after 30 minutes of inactivity
+chrome.alarms.create('discardInactiveTabs', { periodInMinutes: 30 });
+
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+  if (alarm.name === 'discardInactiveTabs') {
+    try {
+      const tabs = await chrome.tabs.query({});
+      const now = Date.now();
+
+      for (const tab of tabs) {
+        if (!tab.active && tab.lastAccessed && (now - tab.lastAccessed) > 30 * 60 * 1000) {
+          await chrome.tabs.discard(tab.id);
+          console.log(`Tab with ID ${tab.id} discarded due to inactivity.`);
+        }
+      }
+    } catch (error) {
+      console.error('Error discarding inactive tabs:', error);
+    }
+  }
+});
